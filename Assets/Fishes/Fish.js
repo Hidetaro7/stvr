61fdb5b2d977c41563ff92e06f0655f920551f5c
Hideki Akiba <hidetaro7@gmail.com>
2015-08-15 23:03:27 +0900

    first commit

---
+++ Assets/Fishes/Fish.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Start () {
+	var x:int = Random.Range(0.0f, 3.0f);
+	var y:int = Random.Range(0.0f, 3.0f);
+	var z:int = Random.Range(0.0f, 3.0f);
+	transform.position = new Vector3(x,y,z);
+	transform.rotation = Quaternion.Euler(0,Random.Range(0f, 180f),0); 
+	//transform.rotation = Random.rotation;
+}
+
+function Update () {
+	 transform.Translate(transform.forward* 0.001);
+}
---
+++ Assets/Main.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+function Start () {
+	for(var i=0; i<20;i++) {
+		Instantiate(Resources.Load("pre_african_blue", GameObject)); 
+	}
+}
+
+function Update () {
+
+} 
